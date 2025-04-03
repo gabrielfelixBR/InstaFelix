8dcf570f7feddc659a1840ba77267d7052bb6999
Felix <mrgabrielfelixsilva@gmail.com>
2025-04-03 10:59:57 -0300

    :tada: Feed e projeto finalizado

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
-import React from "react";
-import { View, Text, StyleSheet} from "react-native";
+import React, {useState} from "react";
+import { View, Text, StyleSheet, FlatList} from "react-native";
 import Header from "./src/Header";
+import List from "./src/List";
 
 export default function App() {
+  const [feed, setFeed] = useState([
+    {
+      id: '1',
+      nome: 'Lucas Silva',
+      descricao: 'Mais um dia de muitos bugs :)',
+      imgPerfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png', 
+      likeada: true,
+      likers: 1,
+    },
+    {
+      id: '2',
+      nome: 'Matheus',
+      descricao: 'Isso sim é ser raiz!!!!!',
+      imgPerfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png',
+      likeada: false,
+      likers: 0,
+    },
+    {
+      id: '3',
+      nome: 'Jose Augusto',
+      descricao: 'Bora trabalhar, hoje estou começando em um projeto novo aqui no sujeito, desde o backend ao frontend',
+      imgPerfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',
+      likeada: false,
+      likers: 3,
+    },
+    {
+      id: '4',
+      nome: 'Gustavo Henrique',
+      descricao: 'Isso sim que é TI!',
+      imgPerfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png',
+      likeada: false,
+      likers: 1,
+    },
+    {
+      id: '5',
+      nome: 'Guilherme',
+      descricao: 'Boa tarde galera do insta...',
+      imgPerfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+      likeada: false,
+      likers: 32,
+    }
+  ])
+
   return (
     <View style={styles.container}>
       <Header/>
+
+      <FlatList
+        data={feed}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <List data={item}/>}
+      />
     </View>
   );
 }
