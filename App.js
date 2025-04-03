8b9b1f34ced461eca96f3a31c48733c4734d3c20
Felix <mrgabrielfelixsilva@gmail.com>
2025-04-03 10:23:36 -0300

    :white_check_mark: Header

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet} from "react-native";
+import Header from "./src/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Header/>
     </View>
   );
 }
